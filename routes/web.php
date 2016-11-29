11ba7abee2c78d0e4e27fc0ea53e1c38f124cb64
Michael Price <mikkyx@gmail.com>
2016-11-29 21:28:38 +0000

    quick analytics route

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,15 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $google = new Google_Client();
+    $google->setApplicationName('Mike Analytics Test');
+    $google->setAuthConfig(resource_path(env('KEY_JSON')));
+    $google->setScopes(['https://www.googleapis.com/auth/analytics.readonly']);
+    $analytics = new Google_Service_Analytics($google);
+
+    dd($analytics->data_realtime->get(
+        'ga:'.env('VIEW_ID'),
+        'rt:activeUsers',
+        ['dimensions' => 'rt:medium']
+    ));
 });
